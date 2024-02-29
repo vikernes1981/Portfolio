8824a39f5bcbd37421ec0c34dd181ab7780a3dab
vikernes1981 <kostaspapas321@gmail.com>
2024-02-29 22:02:21 +0100

    Started chapter 3, work in progress

--- RhelGamePython/chapter3_deploy_config_systems/ChallengeGenerator.py
+++ RhelGamePython/chapter3_deploy_config_systems/ChallengeGenerator.py
@@ -1,27 +1,28 @@
 import slow_validInput
 
-class Challenge:
-    def __init__(self, challenge_number, description, story, options):
-        self.challenge_number = challenge_number
+
+class ChallengeGenerator:
+    def __init__(self, quest_number, description, story, options):
+        self.quest_number = quest_number
         self.description = description
         self.story = story
         self.options = options
 
     def execute(self):
         try:
-            slow_validInput.print_slow("\n\nChallenge {}: {}\n\n".format(self.challenge_number, self.description))
+            slow_validInput.print_slow("\n\nquest {}: {}\n\n".format(self.quest_number, self.description))
             slow_validInput.print_slow(self.story)
 
             count = 0
-            valid_choices = [str(i) for i in range(1, len(self.options) + 1)]
-
+            valid_choices = [str(i) for i in range(1, len(self.options)+1)]
+            valid_choices.append(str(len(self.options)+1))  
             while True:
                 if count == len(self.options):
                     break
                 print("Options:")
                 for i, option in enumerate(self.options, 1):
                     print(f"{i}. {option['name']}")
-                print(f"{len(self.options)+1}. Continue to next challenge\n")
+                print(f"{len(self.options)+1}. Continue to next quest\n")
 
                 choice = slow_validInput.get_valid_input("Enter your choice (1-{}): ".format(len(self.options) + 1), valid_choices)
 
@@ -31,63 +32,9 @@ class Challenge:
                 selected_option = self.options[int(choice) - 1]
                 slow_validInput.print_slow(selected_option['action'])
 
-                if selected_option['function']() == False:
+                if not selected_option['function']():
                     continue
                 slow_validInput.print_slow("{} successfully!\n".format(selected_option['success_message']))
                 count += 1
         except Exception as e:
             print("An error occurred:", e)
-
-
-
-
-# You can define more challenges similarly
-
-if __name__ == "__main__":
-    challenge1.execute()
-
-
-
-
-# Call the challenge function
-# challenge_configure_boot()
-
-def challenge_1_function():
-    return partitions_filesystem.check_disk_space_command()
-
-def challenge_2_function():
-    return partitions_filesystem.check_uuid_command()
-
-def challenge_3_function():
-    return partitions_filesystem.check_disks_partitions_command()
-
-def challenge_4_function():
-    return partitions_filesystem.check_force_partition_check_command()
-
-
-challenge1 = Challenge(
-    challenge_number=1,
-    description="Configure Local Storage",
-    story=("Suddenly, a flashing icon appears on your display, accompanied by a blaring alert signal."
-                   "You quickly access the message, recognizing the emblem of the Central Cybernetic Command."
-                   "The message reads: 'Emergency protocol initiated. Urgent transmission incoming.'"
-                   "With a sense of apprehension, you accept the transmission, bracing yourself for the impending mission."
-                   "A holographic projection materializes before you, displaying the solemn face of a high-ranking officer from the Central Cybernetic Command."
-                   "Their voice resonates with urgency as they address you directly:"
-                   "'Agent Bishop, the stability of the entire cybernetic infrastructure is in jeopardy.'"
-                   "'An anomaly has been detected in the system, one that threatens to disrupt the delicate balance of our digital world.'"
-                   "'You, with your unparalleled expertise in Red Hat administration, are our last hope.'"
-                   "'Your mission is multifaceted, encompassing system diagnostics, filesystem checks, and ensuring data integrity.'"
-                   "Time is of the essence. The fate of humanity rests in your hands.'"
-                   "You nod solemnly, understanding the gravity of the situation."
-                   "With a determined resolve, you affirm your readiness to embark on the mission and restore stability to the cybernetic infrastructure."
-                   "As you navigate through the digital landscape, the urgency of the mission weighs heavily on your mind."
-                   "Every command you execute, every diagnostic tool you deploy, is a step towards ensuring the survival of humanity in the digital age."
-                   "The fate of countless lives rests on your shoulders, and failure is not an option."),
-    options=[
-        {'name': 'List Disk Space', 'action': "You initiate the process to list disk space, a crucial step in understanding the resources available in this digital domain.", 'function': challenge_1_function, 'success_message': 'Disk space listed'},
-        {'name': 'Get UUIDs', 'action': "With determination, you delve into the depths of the system, seeking to retrieve the UUIDs of the disks.", 'function': challenge_2_function, 'success_message': 'UUIDs retrieved'},
-        {'name': 'List Disks and Partitions', 'action': "You navigate the intricate web of disks and partitions, unraveling their secrets one by one.", 'function': challenge_3_function, 'success_message': 'Disks and partitions listed'},
-        {'name': 'Force Partition Check', 'action': "With unwavering resolve, you command a forceful partition check, ensuring the stability of the digital landscape.", 'function': challenge_4_function, 'success_message': 'Partition check forced'}
-    ]
-)
--- RhelGamePython/chapter3_deploy_config_systems/CommandGenerator.py
+++ RhelGamePython/chapter3_deploy_config_systems/CommandGenerator.py
@@ -1,9 +1,10 @@
 import slow_validInput
 
 class CommandGenerator:
-    def __init__(self, action, correct_command, hint, intro_text, outro_text, command_options=None, command_output=None):
+    def __init__(self, action, correct_command, command_aspects, hint, intro_text=[], outro_text=[], command_options=None, command_output=None):
         self.action = action
         self.correct_command = correct_command
+        self.command_aspects = command_aspects
         self.hint = hint
         self.intro_text = intro_text
         self.outro_text = outro_text
@@ -12,7 +13,7 @@ class CommandGenerator:
 
     def execute(self):
         try:
-            slow_validInput.print_slow(self.intro_text)
+            slow_validInput.print_slow(self.intro_text[0])
             while True:
                 user_input = input(f"Enter the command to {self.action} or type 'quit/q' to exit: ").strip()
                 if user_input.lower() in ['quit', 'q']:
@@ -35,6 +36,10 @@ class CommandGenerator:
         print("\nCommand is correct. Here are the aspects of the command:")
         print("Command:", user_input)
         print("Purpose:", self.action.capitalize())
+        if self.command_aspects:
+            print("\nAdditional Aspects:")
+            for aspect in self.command_aspects:
+                print(aspect)
         if self.command_options:
             print("\nOptions:")
             for option in self.command_options:
@@ -43,44 +48,5 @@ class CommandGenerator:
             print("\nOutput Example:")
             for output in self.command_output:
                 print(output)
-        slow_validInput.print_slow(self.outro_text)
+        slow_validInput.print_slow(self.outro_text[0])
         print("\nYou can continue with the game.")
-
-
-
-
-
-# Example usage
-
-
-intro_text = ("As you continue your journey through storage management, you encounter scenarios where certain storage devices need to be retired or replaced."
-              "Removing physical volumes associated with these devices is crucial to maintain the integrity and efficiency of the storage infrastructure."
-              "To address this, you embark on a quest to safely remove the designated physical volumes.\n"
-              "With careful consideration, you assess the implications of removing the physical volumes."
-              "You review the volume group configurations and ensure that removing the physical volumes will not compromise data integrity or system performance.\n"
-              "Issuing commands to remove the physical volumes, you proceed cautiously, following best practices to minimize risks."
-              "As the commands execute, you monitor the removal process, ensuring that each physical volume is detached safely.\n")
-
-outro_text = (f"After the remove command completes, you verify the system's status to confirm the successful removal of the designated physical volumes."
-              f"You check for any errors or warnings, ensuring that the storage infrastructure remains stable and operational."
-              f"As your quest to remove physical volumes concludes, you reflect on the journey."
-              f"Through careful planning and execution, you've successfully removed designated storage devices."
-              f"With the physical volumes removed, you're poised to maintain the integrity and efficiency of the storage infrastructure.\n")
-
-command_options = ["-f, --force\t\tForce removal of the physical volume.",
-                   "-y, --yes\t\tAssume 'yes' as answer to all questions.",
-                   "-v, --verbose\t\tProvide verbose output."]
-
-command_output = ["Labels on physical volume \"/dev/sdb1\" successfully wiped."]
-
-pv_remove_command =CommandGenerator.CommandGenerator(
-    action="remove",
-    correct_command="pvremove /dev/sdb1",
-    hint="Hint: Use the 'pvremove' command followed by the device name to remove a physical volume (e.g., pvremove /dev/sdb1).",
-    intro_text=intro_text,
-    outro_text=outro_text,
-    command_options=command_options,
-    command_output=command_output
-)
-
-# pv_remove_command.execute()
